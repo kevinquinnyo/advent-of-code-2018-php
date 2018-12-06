4c68a925ef74ef66176f95b1ee2c60fc9395cf0b
Kevin Quinn <kevin.quinn@totalserversolutions.com>
2018-12-06 00:19:45 -0500

    add script

---
+++ day3.php
@@ -0,0 +1,11 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use kevinquinnyo\AdventOfCode\Fabric;
+
+echo 'PART 1:' . PHP_EOL;
+$answer = (new Fabric)
+    ->buildMatrix()
+    ->process()
+    ->getOverlappingArea();
+echo sprintf('ANSWER: %d', $answer) . PHP_EOL;
